4e9978ddb1d15a334d604f1f6acbdecedcac9e08
Sadewa <64367887+sadewa1609@users.noreply.github.com>
2024-03-22 00:51:12 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-67c81a36-4578-49bb-a8ef-5603c84e885f/)
 
 
 echo "$curl_output" >> Update.tsx
